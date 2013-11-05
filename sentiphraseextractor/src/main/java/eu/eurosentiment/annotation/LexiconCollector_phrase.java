2858e213ef902971d6f49e06b61aea2d72c39df0
Kartik <asooja@gmail.com>
2013-11-05 13:26:43 +0000

    Added load folder and sentiwordnet file

---
+++ sentiphraseextractor/src/main/java/eu/eurosentiment/annotation/LexiconCollector_phrase.java
@@ -0,0 +1,84 @@
+package eu.eurosentiment.annotation;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import eu.monnetproject.clesa.core.utils.BasicFileTools;
+
+public class LexiconCollector_phrase {
+
+	public static void main(String[] args) {
+		String dirPath = "src/main/resources/output/new";
+		Map<String, Double> mentionPhraseScoreMap = new HashMap<String, Double>();
+		File dir = new File (dirPath);
+		File[] files = dir.listFiles();
+		for(File file : files){
+			BufferedReader reader = BasicFileTools.getBufferedReader(file);
+			String line = null;
+			try {
+				while((line=reader.readLine())!=null){
+					String[] split = line.split("\t");
+					String mention = split[0].replace(","," ");
+					String sentimentPhrase = split[2].replace(","," ");
+					String scoreString = split[3];
+					if(!scoreString.contains("null") && mention.length()>2){
+						try{
+							Double score = Double.parseDouble(split[3]);
+							if(mentionPhraseScoreMap.get(mention+"-----"+sentimentPhrase) == null){								
+								mentionPhraseScoreMap.put(mention+"-----"+sentimentPhrase, score);								
+							} else {
+								Double savedScore = mentionPhraseScoreMap.get(mention+"-----"+sentimentPhrase);
+								double avgScore = (savedScore + score) / 2; 
+								mentionPhraseScoreMap.put(mention+"-----"+sentimentPhrase, avgScore);
+							}					
+						} catch(Exception e){
+						}
+					}
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		StringBuffer buffer = new StringBuffer();
+
+		Map<String, List<String>> phraseMentionMap = new HashMap<String, List<String>>();
+		
+		for(String mentionPhrase : mentionPhraseScoreMap.keySet()){
+			String[] split = mentionPhrase.split("-----");
+			String mention = split[0].toLowerCase();
+			String phrase = split[1].toLowerCase();
+			if(phraseMentionMap.get(phrase)==null)
+				phraseMentionMap.put(phrase, new ArrayList<String>());
+			phraseMentionMap.get(phrase).add(mention);			
+		}
+		
+		for(String phrase : phraseMentionMap.keySet()){
+			List<String> mentions = phraseMentionMap.get(phrase);
+			for(String mention : mentions){
+				Double score = mentionPhraseScoreMap.get(mention + "-----" + phrase);
+				buffer.append(mention + "\t,\t" + phrase + "\t,\t" + score);
+				buffer.append("\n");
+			}
+		}
+		
+//		for(String mentionPhrase : mentionPhraseScoreMap.keySet()){
+//			String[] split = mentionPhrase.split("-----");
+//			String mention = split[0].toLowerCase();
+//			String phrase = split[1].toLowerCase();
+//			Double score = mentionPhraseScoreMap.get(mentionPhrase);
+//			buffer.append(mention + "\t,\t" + phrase + "\t,\t" + score);
+//			buffer.append("\n");
+//		}
+
+		BasicFileTools.writeFile("src/main/resources/finalOutput_new_avgScored_keyPhrase.txt", buffer.toString().trim());
+
+	}	
+
+
+}
